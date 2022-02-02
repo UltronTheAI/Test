0e6e8a3586d7fa6b8bcbdd1a152720785f875967
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-02-01 22:08:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,24 @@ function data (dt) {
             return "Name, Password error";
         }
     }
+    if (mode_ == "sign_up") {
+        d[name] = {};
+        d[name].password = password;
+        return "No Error";
+    }
+    if (mode_ == "acc_remove") {
+        var np = d[name].password;
+        if (password == np) {
+            delete d[name];
+            return "No Error";
+        }
+        if (np == undefined) {
+            return "Name, Password error";
+        }
+        else {
+            return "Name, Password error";
+        }
+    }
 
     return st;
 }
