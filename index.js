5cf1f003d0dd7b6adc02f1afb191c60a29124064
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-02-01 21:10:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const app = express();
 
 var d = {"Server": {"passwords": "9876.server.5000"}};
 
-function jlts (dt) {
-    var st = "";
-    for (var i = 0; i < dt.length; i++) {
-        st += dt[i];
-    }
-    return st;
-}
 
 function data (dt) {
     dt = dt.split("");
     delete dt[0];
     delete dt[0];
-    dt = jlts(dt);
-    
-    return dt;
+
+    var st = "";
+    for (var i = 0; i < dt.length; i++) {
+        st += dt[i];
+    }
+
+    return st;
 }
 
 app.get('/', (req, res) => {
