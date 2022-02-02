a7abc661aedf2aa3f4da7a1443bf106059423784
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-02-01 20:50:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 
+const fs = require('fs');
 var d = [];
 
 function data (dt) {
     if (dt == "/?hi") {
-        d.push(String(dt).replace('/?', ''));
-        return d;
+        fs.writeFile("books.txt", dt, (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("File written successfully\n");
+            console.log("The written has the following contents:");
+            console.log(fs.readFileSync("books.txt", "utf8"));
+        }
+        });
+        setTimeout(() => {
+            fs.readFile('books.txt', 'utf8', function(err, data){
+            
+                // Display the file content
+                return d;
+            });
+        }, 1000);
     }
     return "hi_-8";
 }
